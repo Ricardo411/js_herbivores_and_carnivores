0e6096d54c7c915da8f46adb9862aff9a616ca01
Ricardo Guimarães Borges <107629307+Ricardo411@users.noreply.github.com>
2025-08-14 12:41:09 -0300

    add solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    // Remover usando filter, conforme exigido
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    // Checar apenas se é Herbivore e não está escondido
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+
+      // Se a saúde for <= 0, chamar die
+      if (target.health <= 0) {
+        target.die();
+      }
+    }
+  }
 }
 
 module.exports = {
